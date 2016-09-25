bdde1e1cf264501083a72112b8a363280eae1851
mtochs <mtochs@gmail.com>
2016-09-24 20:32:39 -0600

    converted from dtc to skb.  improved enron functions.  updated formating.

--- Enron Finance and Email Analysis with Machine Learning/poi_id.py
+++ Enron Finance and Email Analysis with Machine Learning/poi_id.py
@@ -9,7 +9,6 @@ import numpy as np
 from sklearn import preprocessing
 from sklearn import tree
 from sklearn.cross_validation import train_test_split
-from sklearn.cluster import KMeans
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
@@ -21,85 +20,101 @@ from feature_format import featureFormat, targetFeatureSplit
 import new_features
 from scatplot import scatplot
 from tester import dump_classifier_and_data
-import iter_function
+import enron_function
 
 def mean(numbers):
     return float(sum(numbers)) / max(len(numbers), 1)
 
-### Task 1: Select features
-### features_list is a list of strings, each of which is a feature name.
-### The first feature must be "poi".
-### 
-### Financial features: all units are in US dollars
+"""
+   Task 1: Select features
+   features_list is a list of strings, each of which is a feature name.
+   The first feature must be "poi".
+   
+   Financial features: all units are in US dollars
+   
+   Email feature: units are generally number of emails messages;
+   notable exception is ‘email_address’, which is a text string
+"""
 poi_label = ['poi']
-financial_features = ['salary',
-                      'deferral_payments',
-                      'total_payments',
-                      'loan_advances',
-                      'bonus',
-                      'restricted_stock_deferred',
-                      'deferred_income',
-                      'total_stock_value',
-                      'expenses',
-                      'exercised_stock_options',
-                      'long_term_incentive',
-                      'restricted_stock',
-                      'director_fees',
-                      'total_comp'] #created from 'new_features' function
-### Units are generally number of emails messages;
-### notable exception is ‘email_address’, which is a text string
-email_features = ['fraction_to_poi'] #created from 'new_features' function
+financial_features = [                      
+    #'total_comp' #created from 'new_features' function
+    'salary',
+    'deferral_payments',
+    'total_payments',
+    'loan_advances',
+    'bonus',
+    'restricted_stock_deferred',
+    'deferred_income',
+    'total_stock_value',
+    'expenses',
+    'exercised_stock_options',
+    'long_term_incentive',
+    'restricted_stock',
+    'director_fees'
+    ]
+email_features = [
+    #'fraction_to_poi', #created from 'new_features' function
+    'from_messages',
+    'from_poi_to_this_person',
+    'from_this_person_to_poi',
+    'shared_receipt_with_poi',
+    'to_messages'
+    ]
 features_list = poi_label + financial_features + email_features
 
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
     data_dict = pickle.load(data_file)
 
-### Task 2: Remove outliers
+"""Task 2: Remove outliers """
 data_dict.pop('TOTAL', 0)
 data_dict.pop('THE TRAVEL AGENCY IN THE PARK', 0)
 #scatplot(data_dict, 'salary', 'bonus')
 
-### Task 3: Create new feature(s)
-### Store to my_dataset for easy export below.
+"""Task 3: Create new feature(s)
+   Store to my_dataset for easy export below. """
 my_dataset = copy(data_dict)
 my_dataset = new_features.add_poi_email_fraction(my_dataset)
 my_dataset = new_features.total_comp(my_dataset)
 
 
-### This section identifies the top 8 features in features_list and disregards
-### all other features.  The classifier will proceed using the reduced
-### features list below.
-features_list_post_DTC = iter_function.top_features(my_dataset, features_list, 1000, 8)
-print "New Features List: ", features_list_post_DTC
+"""This section identifies the top 8 features in features_list and disregards
+   all other features.  The classifier will proceed using the reduced
+   features list below. """
+skb_features = enron_function.skb(data_dict, features_list, 8)
+features_list_SKB = poi_label + skb_features
+print "New Features List: ", features_list_SKB
+
+
 
 ### Extract features and labels from dataset for local testing
-data = featureFormat(my_dataset, features_list_post_DTC, sort_keys=True)
+data = featureFormat(my_dataset, features_list_SKB, sort_keys=True)
 labels, features = targetFeatureSplit(data)
 
+"""Feature scaling with MinMaxScaler """
+scaler = preprocessing.MinMaxScaler()
+features = scaler.fit_transform(features)
 
-### Task 4: Try a varity of classifiers
+"""Task 4: Try a varity of classifiers """
 clf_gaussian = GaussianNB()
-clf_kmeans = KMeans(n_clusters=2)
-clf_svc = SVC(kernel='rbf', C=100)
+clf_svc = SVC(kernel='rbf', C=1000)
 
-clf = clf_gaussian
+clf = clf_svc
 
-### Task 5: Tune classifier to achieve better than .3 precision and recall 
-### using our testing script. Check the tester.py script in the final project
-### folder for details on the evaluation method, especially the test_classifier
-### function. Because of the small size of the dataset, the script uses
-### stratified shuffle split cross validation. For more info: 
-### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
-
-accuracy, precision, recall = iter_function.clf_stats(clf, features, labels, 1000)
+"""Task 5: Tune classifier to achieve better than .3 precision and recall 
+   using our testing script. Check the tester.py script in the final project
+   folder for details on the evaluation method, especially the test_classifier
+   function. Because of the small size of the dataset, the script uses
+   stratified shuffle split cross validation. For more info: 
+   http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html """
+accuracy, precision, recall = enron_function.clf_stats(clf, features, labels, 1000)
 print "Accuracy score: ", accuracy
 print "Precision: ", precision
 print "Recall: ", recall
 
-### Task 6: Dump your classifier, dataset, and features_list so anyone can
-### check your results. You do not need to change anything below, but make sure
-### that the version of poi_id.py that you submit can be run on its own and
-### generates the necessary .pkl files for validating your results.
 
+"""Task 6: Dump your classifier, dataset, and features_list so anyone can
+   check your results. You do not need to change anything below, but make sure
+   that the version of poi_id.py that you submit can be run on its own and
+   generates the necessary .pkl files for validating your results."""
 dump_classifier_and_data(clf, my_dataset, features_list)
---
+++ Enron Finance and Email Analysis with Machine Learning/tools/enron_function.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+
+from pprint import pprint
+from sklearn.cross_validation import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+
+
+from feature_format import featureFormat, targetFeatureSplit
+
+def mean(numbers):
+    return float(sum(numbers)) / max(len(numbers), 1)
+
+
+"""
+   Function utilizes SelectKBest to identify top features for a given data set.
+"""
+def getKey(item):
+    return item[1]
+
+from sklearn.feature_selection import SelectKBest
+def skb(data_dict, features_list, k):
+    data = featureFormat(data_dict, features_list)
+    labels, features = targetFeatureSplit(data)
+    clf = SelectKBest(k=k)
+    clf.fit(features, labels)
+    scores = clf.scores_
+    f = zip(features_list[1:], scores)
+    f = list(sorted(f, key=getKey, reverse=True))
+    for n in range(k):
+        print "#{} {} with score {}".format(n+1, f[n][0], f[n][1])
+    return dict(f[:k]).keys()
+
+
+"""
+Function runs through mutliple classifying iterations to get a better reading
+on accuracy, precision, and recall.
+"""
+def clf_stats(clf, features, labels, iterations=1000):
+    accuracy = []
+    precision = []
+    recall = []
+    for n in range(iterations):
+        features_train, features_test, labels_train, labels_test = \
+            train_test_split(features, labels, test_size=0.3, random_state=42)
+        clf.fit(features_train, labels_train)
+        pred = clf.predict(features_test)
+        accuracy.append(accuracy_score(labels_test, pred))
+        precision.append(precision_score(labels_test, pred))
+        recall.append(recall_score(labels_test, pred))
+    
+    return mean(accuracy), mean(precision), mean(recall)
+    
+    
+    
+    
+    
+    
+    
+    
+    
